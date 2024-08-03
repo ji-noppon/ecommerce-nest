c00fbe9ef7e62e4ded9628d01fde08cbc3b217d1
Np <you@example.com>
2024-08-04 02:50:27 +0700

    init project

---
+++ src/product/product.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Injectable()
+export class ProductService {
+  productList = {
+    name: 'shirt',
+    price: 200,
+  };
+
+  create(createProductDto: CreateProductDto) {
+    return 'This action adds a new product';
+  }
+
+  findAll() {
+    return this.productList;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
