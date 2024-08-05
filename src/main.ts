740e5a4c37174fddc6c5eba060f113c97b94dcd1
Np <you@example.com>
2024-08-05 08:35:04 +0700

    [fix]: swagger on production

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Serve static files (like Swagger UI assets)
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const config = new DocumentBuilder()
     .setTitle('Api Document')
