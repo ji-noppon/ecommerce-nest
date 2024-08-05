4a684f0d8673ccaf7f18f4fc3c19d6c0f9ab9e37
Np <you@example.com>
2024-08-05 08:49:28 +0700

    [fix]: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document, {
+  SwaggerModule.setup('api', app, document, {
     customSiteTitle: 'Backend Generator',
     customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [
@@ -30,8 +30,6 @@ async function bootstrap() {
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
     ],
   });
-
-  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
